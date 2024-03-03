9a7b5610f91d8dccc8b7dd6628015df8a02677ca
Aditya Braganza <87462183+adityabraganza@users.noreply.github.com>
2024-03-03 18:49:54 +0530

    a

--- src/NativeFunctions/data.java
+++ src/NativeFunctions/data.java
@@ -8,16 +8,23 @@ public class data {
         String[] dataGiven = NSL_regular_season_data_2.dataString;
         game[] returnArr = new game[dataGiven.length];
 
+        if (dataGiven[12] == null){
+
+        }
+
+            //12 & 13, 0.76
+
         for (int i = 0; i < dataGiven.length; i++){
             String[] values = dataGiven[i].split(",");
+
             returnArr[i] = new game(
                     Integer.parseInt(values[0].split("_")[2]),
                     values[1],
                     values[2],
                     Integer.parseInt(values[3]),
                     Integer.parseInt(values[4]),
-                    Double.parseDouble(values[5]),
-                    Double.parseDouble(values[6])
+                    Double.parseDouble(values[5]) - (0.76 * Double.parseDouble(values[12])),
+                    Double.parseDouble(values[6]) - (0.76 * Double.parseDouble(values[13]))
             );
         }
 
